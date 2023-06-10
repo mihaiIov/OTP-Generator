10f2051890666ead942275ca172940560576e837
Mihai Iov <iov_mike@yahoo.co.uk>
2023-06-10 18:24:34 +0300

    Add project files.

---
+++ OTP handler/Handler.cs
@@ -0,0 +1,16 @@
+﻿namespace OTP_handler
+{
+    public static class Handler
+    {
+        public static string GenerateOTP(string userId, DateTime dateTime)
+        {
+            
+            throw new NotImplementedException();
+        }
+        public static bool AttemptValidation(string otp) 
+        {
+            throw new NotImplementedException ();
+        }
+
+    }
+}
